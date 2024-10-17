43062e0d861a9b1051868860ddadc845c2d65455
Manjo <kksagesse@gmail.com>
2024-10-17 19:07:08 +0200

    add script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-
+var form = document.getElementsByTagName("form")[0];
 var firstname = document.getElementById("first-name");
 var lastname = document.getElementById("last-name");
 var message = document.getElementById("message");
@@ -10,12 +10,13 @@ var commentlist = document.getElementById("comment-list");
 
 
 
-Form.addEventListener('submit', function(event){ 
+form.addEventListener(
+    "submit", function(event){ 
     var flag= false;
 	flag=flag || (firstname.value=="");
 	flag=flag || (lastname.value=="");
 	flag=flag || (message.value.trim() === "");
-	if (flag = true)
+	if (flag == true)
 	{
 		error.style.display='block';
 		event.preventDefault()
@@ -25,30 +26,28 @@ Form.addEventListener('submit', function(event){
     
  
         else{
-            let Div1 = document.createElement('Div1'); // on cree une div1
-            let newContent = document.createTextNode;
-            Div1.appendChild(newContent);
-            Div1.classList.add('prose', 'prose-sm','mt-4', 'max-w-none', 'text-gray-500Content');
+            error.style.display='none';
+            let paragraf = document.createElement('div');
+            paragraf.textContent = message.value;
+            paragraf.classList.add('prose', 'prose-sm','mt-4', 'max-w-none', 'text-gray-500');
+            let nom= document.createElement('h3');
+            nom.textContent = firstname.value+" "+ lastname.value;
+            nom.classList.add('font-medium' ,'text-gray-900');
+            let comment = document.createElement('div');
+            comment.classList.add('flex-1', 'py-10','border-t','border-gray-200');
+            comment.appendChild(nom);
+            comment.appendChild(paragraf);
+            let commentdiv= document.createElement('div');
+            commentdiv.classList.add('fflex', 'space-x-4' ,'text-sm' ,'text-gray-500');
+            commentdiv.appendChild(comment)
+            commentlist.appendChild(commentdiv);
+            form.reset();
+            }
+        
+        
     
-            let Div2 = document.createElement('Div2');
-            let newContent2 = document.createTextNode;
-            newContent2 = firstNameInput.value + "" + lastNameInput.value;
-            newContent2.classList.add('font-medium', 'text-gray-900');
-            Div2.appendChild(newContent2);
-            Div2.classList.add('flex-1', 'py-10','border-t','border-gray-200');
-    
-            Div2.appendChild(Div1);
-    
-            let Div = document.createElement('Div');
-            Div.classList.add('flex','space-x-4',' text-sm',' text-gray-500');
-            Div.appendChild(Div2);
-            comment-list.appendChild(Div2);
-    
-    
-    
-        }
-    },false);
-    
+    },false,
+    );
     
                                       
     
